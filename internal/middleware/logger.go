0f7c5ea283de54bc98a6c030c758ad26dcb7569a
swiftbird07 <48106483+swiftbird07@users.noreply.github.com>
2024-06-07 22:46:20 +0200

    Add more logging

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -6,12 +6,28 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Get source IP for logging
+		var sourceIP string
+		if forwardedFor := r.Header.Get("X-Forwarded-For"); forwardedFor != "" {
+			sourceIP = strings.Split(forwardedFor, ",")[0]
+		} else if realIP := r.Header.Get("X-Real-IP"); realIP != "" {
+			sourceIP = realIP
+		} else {
+			sourceIP = r.RemoteAddr
+		}
+
 		_, isDebug := os.LookupEnv("DEBUG")
 		if !isDebug {
+			// Log remote Ip and real IP of the client if they differ so analysis can be done if needed
+			if sourceIP != r.RemoteAddr {
+				log.Printf("Buddy Tracker Server - Parsed IP Information - Remote Connection %s has real IP: %s\n", r.RemoteAddr, sourceIP)
+			}
+
 			next.ServeHTTP(w, r)
 			return
 		}
@@ -32,7 +48,7 @@ func Logger(next http.Handler) http.Handler {
 		}
 		log.SetOutput(os.Stdout)
 
-		log.Printf("\nRemoteAddr: %s\nURL: %s\nHeader: %s\nCookies: %s\nBody: %s\n", r.RemoteAddr, r.URL.String(), string(bHeader), string(bCookies), string(bData))
+		log.Printf("\nRemoteAddr: %s\nParsed IP: %s\nURL: %s\nHeader: %s\nCookies: %s\nBody: %s\n", r.RemoteAddr, sourceIP, r.URL.String(), string(bHeader), string(bCookies), string(bData))
 
 		next.ServeHTTP(w, r)
 	})
