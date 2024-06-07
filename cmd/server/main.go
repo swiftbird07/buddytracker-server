c741dc2ae48af3f327a6547d8c6ae8007ed7a4b5
swiftbird07 <48106483+swiftbird07@users.noreply.github.com>
2024-06-07 22:20:00 +0200

    Add log on start

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,6 +9,8 @@ import (
 )
 
 func main() {
+	log.Println("Buddy Tracker Server - Starting server on port 3001")
+
 	r := router.NewRouter()
 
 	err := http.ListenAndServe(fmt.Sprintf("%s:%d", "0.0.0.0", 3001), r)
