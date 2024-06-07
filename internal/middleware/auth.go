ab4e83e30904c212131e520103a0572a22c8e521
swiftbird07 <48106483+swiftbird07@users.noreply.github.com>
2024-06-07 22:02:24 +0200

    Fix missing import

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"strings"
 
@@ -24,7 +25,7 @@ func Auth(next http.Handler) http.Handler {
 
 		// Check if User Agent starts with "Buddy Tracker"
 		if !strings.HasPrefix(r.UserAgent(), "Buddy Tracker") {
-			log.Println("Buddy Tracker Server - Auth Fail - User-Agent mismatch for incoming connection (Source IP:", sourceIP + ". User-Agent:", r.UserAgent() + ").")
+			log.Println("Buddy Tracker Server - Auth Fail - User-Agent mismatch for incoming connection (Source IP: ", sourceIP+". User-Agent: ", r.UserAgent()+").")
 			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
 			return
 		}
@@ -33,20 +34,21 @@ func Auth(next http.Handler) http.Handler {
 		authHeader := r.Header.Get("Authorization")
 		token, found := strings.CutPrefix(authHeader, "Bearer ")
 		if !found {
-			log.Println("Buddy Tracker Server - Auth Fail - No token found in Authorization header for incoming connection (Source IP:", sourceIP+ ". User-Agent:", r.UserAgent() + ").")
+			log.Println("Buddy Tracker Server - Auth Fail - No token found in Authorization header for incoming connection (Source IP: ", sourceIP+". User-Agent: ", r.UserAgent()+").")
 			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
 			return
 		}
-		
+
 		// Validate token
 		validSession, user := controller.ValidToken(token)
 
 		if !validSession {
-			log.Println("Buddy Tracker Server - Auth Fail - Authentication failed for incoming connection (Source IP:", sourceIP + ". User-Agent:", r.UserAgent() + ").")
+			log.Println("Buddy Tracker Server - Auth Fail - Authentication failed for incoming connection (Source IP: ", sourceIP+". User-Agent: ", r.UserAgent()+").")
 			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
 			return
 		} else {
-			log.Println("Buddy Tracker Server - Auth Success - Authentication successful for incoming connection. (User:", user + ", Source IP:", sourceIP + ". User-Agent:", r.UserAgent() + ").")
+			// Get username
+			log.Println("Buddy Tracker Server - Auth Success - Authentication successful for incoming connection. (User: ", user.Name+", Source IP: ", sourceIP+". User-Agent: ", r.UserAgent()+").")
 			ctx := context.WithValue(r.Context(), Key("user"), user)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		}
